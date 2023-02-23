e970414a5c3620ce84119816b99e2e41cda6e36f
Tanzim Ahmed <112745212+Tanzz4@users.noreply.github.com>
2023-02-23 22:36:37 +0600

    Assignment 01
    This is an Object Oriented Programming Assignment 01

---
+++ Assignment_01.py
@@ -0,0 +1,10 @@
+
+id = input("Enter your ID :")
+Total_work_hour_in_a_month = float(input("Total work hour in a month : "))
+Amount_received_per_hour = float(input("Amount received per hour : "))
+print("Id is :",id)
+print("Salary is %.2f"%(Total_work_hour_in_a_month*Amount_received_per_hour))
+
+
+#Write a Python program that accepts an employee's ID, total worked hours of a month and the amount S/he received per hour.
+# Print the employee's ID and salary (with two decimal places) of a particular month. S/he has day off on Friday and Saturday.
